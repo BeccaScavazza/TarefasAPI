2114373b46509db4c8a3b359003f58169be1d7fd
Rebecca Scavazza <rebecca.sdionisio@gmail.com>
2022-12-01 09:55:05 -0300

    Criacao de API de Tarefas

---
+++ SistemaDeTarefas/Controllers/TarefasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaDeTarefas.Controllers
+{
+    public class TarefasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
